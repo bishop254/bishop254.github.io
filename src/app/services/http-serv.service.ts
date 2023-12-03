44f6d2e0ec5ec1eb701fff37218745d577f5f793
Karanja.Chiuri <karanjachiuri@gmail.com>
2023-12-03 16:50:49 +0300

    Http Headers Fix

--- src/app/services/http-serv.service.ts
+++ src/app/services/http-serv.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
@@ -8,12 +8,14 @@ import { Observable } from 'rxjs';
 export class HttpServService {
   constructor(private http: HttpClient) {}
 
+  private genHeaders(): HttpHeaders {
+    return new HttpHeaders({ 'ngrok-skip-browser-warning': 'true' });
+  }
+
   public postReq(endpoint: string, model: any): Observable<any> {
     endpoint = 'https://0e16-41-90-65-6.ngrok-free.app/bursary/register';
     return this.http.post(endpoint, model, {
-      headers: {
-        'ngrok-skip-browser-warning': 'true',
-      },
+      headers: this.genHeaders(),
     });
-  } 
+  }
 }
